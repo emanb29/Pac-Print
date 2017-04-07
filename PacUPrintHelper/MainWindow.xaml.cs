ebef5de9ec598f355232de83fe0dd34d25de5eef
emanb29 <ebgamer29@gmail.com>
2017-04-07 01:15:33 -0700

    Write MVP

--- PacUPrintHelper/MainWindow.xaml.cs
+++ PacUPrintHelper/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using FolderBrowserDialog = System.Windows.Forms.FolderBrowserDialog;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace PacUPrintHelper
 {
@@ -20,9 +23,40 @@ namespace PacUPrintHelper
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private FolderBrowserDialog cInDirDialog = new FolderBrowserDialog();
+		private SaveFileDialog cOutFileDialog = new SaveFileDialog();
+		private ViewModel cCtx;
+
+		private void RunBtn_Click(object sender, RoutedEventArgs e)
+		{
+			cCtx.DoMagic();
+			return;
+		}
+
 		public MainWindow()
 		{
+			cCtx = new ViewModel();
 			InitializeComponent();
+			this.DataContext = cCtx;
+		}
+
+		private void InDirBtn_Click(object sender, RoutedEventArgs e)
+		{
+			var result = cInDirDialog.ShowDialog();
+			if (result == System.Windows.Forms.DialogResult.OK || result == System.Windows.Forms.DialogResult.Yes)
+			{
+				cCtx.cInFolder = cInDirDialog.SelectedPath;
+				cCtx.ListFilesInDialog();
+			}
+		}
+
+		private void OutDirBtn_Click(object sender, RoutedEventArgs e)
+		{
+			var result = cOutFileDialog.ShowDialog();
+			if (result.GetValueOrDefault(false))
+			{
+				cCtx.cOutFile = cOutFileDialog.FileName;
+			}
 		}
 	}
 }
---
+++ PacUPrintHelper/ViewModel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PacUPrintHelper
+{
+	class ViewModel : INotifyPropertyChanged
+	{
+		public class SourceFile
+		{
+			private string filePath;
+			public static Collection<String> SourceExtensions = new Collection<string>() {"c", "cpp", "h", "hpp"};
+
+			public String getFullyQualifiedName()
+			{
+				return filePath;
+			}
+
+			public String getFileName()
+			{
+				return Regex.Match(filePath, "^.*\\\\(.*\\..*)$").Groups[1].ToString();
+			}
+
+			public String getExtension()
+			{
+				return Regex.Match(filePath, "^.*\\.(.*)$").Groups[1].ToString();
+			}
+
+			public SourceFile(string path)
+			{
+				filePath = path;
+			}
+
+			public override string ToString()
+			{
+				return getFileName();
+			}
+
+			//Converting constructor
+			public static implicit operator SourceFile(string path)
+			{
+				return new SourceFile(path);
+			}
+		}
+
+		private string inFolder;
+
+		public String cInFolder
+		{
+			get { return inFolder; }
+			set
+			{
+				inFolder = value;
+				OnPropertyChanged();
+			}
+		}
+
+		private string outFile;
+
+		public String cOutFile
+		{
+			get { return outFile; }
+			set
+			{
+				outFile = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public ObservableCollection<SourceFile> cFileNames { get; set; } = new ObservableCollection<SourceFile>();
+
+		public ViewModel()
+		{
+			cInFolder = "";
+			cOutFile = "";
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public void ListFilesInDialog()
+		{
+			cFileNames.Clear();
+			var fileList = Directory.GetFiles(cInFolder);
+			for (var i = fileList.Length - 1; i >= 0; --i)
+			{
+				SourceFile cFile = fileList[i];
+				var ext = cFile.getExtension();
+
+				if (SourceFile.SourceExtensions.Contains(cFile.getExtension()))
+				{
+					cFileNames.Add(cFile);
+				}
+			}
+		}
+
+		public void DoMagic()
+		{
+			if (cFileNames.Count > 0 && cOutFile.Length > 0)
+			{
+				var outFile = new System.IO.StreamWriter(cOutFile);
+				foreach (var file in cFileNames)
+				{
+					int lineNum = 0;
+					var inFile = new System.IO.StreamReader(file.getFullyQualifiedName());
+					string line;
+					while ((line = inFile.ReadLine()) != null)
+					{
+						outFile.WriteLine(line);
+						++lineNum;
+						if (line.Length > 80)
+						{
+							lineNum += line.Length / 80;
+						}
+					}
+					inFile.Close();
+					while (lineNum % 52 != 0)
+					{
+						outFile.WriteLine();
+						++lineNum;
+					}
+				}
+				outFile.Close();
+			}
+		}
+	}
+}
